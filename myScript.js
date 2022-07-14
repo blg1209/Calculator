17d4777874e705179c149de2ed3b3faa564a058f
Brandon Work <blg1209@gmail.com>
2022-07-13 20:19:31 -0500

    working calcualtion when stringing multiple operands and operators together. Display works as it should. Need to get other buttons working and add checeks for 0

--- myScript.js
+++ myScript.js
@@ -5,21 +5,31 @@ let secondNum = "";
 let operator = "";
 let secondOperator = "";
 let displayValue = 0;
-let total = null;
+let total = "";
 
 buttons.forEach((button) => {
   button.addEventListener('click', function() {
     if(button.classList.contains('operator')){
-        console.log(getOperator(button))
-        console.log(updateDisplay(button))
+        console.log(getOperator(button));
+        if(secondOperator){
+            compute(firstNum, secondNum, operator);
+            updateScreen()
+            firstNum = total;
+            secondNum = "";
+            operator = secondOperator;
+            secondOperator = "";
+        }
+        else{
+            updateScreen()
+        }
     }
     if(button.classList.contains('operand')){
         console.log(appendNum(button))
-        console.log(updateDisplay(button))
+        updateScreen()
     }
     if(button.classList.contains('clear')){
         console.log(clear())
-        console.log(deleteScreen())
+        updateScreen()
     }
     if(button.classList.contains('sign')){
         console.log('Working Sign')
@@ -32,7 +42,7 @@ buttons.forEach((button) => {
     }
     if(button.classList.contains('equal')){
         compute(firstNum, secondNum, operator)
-        updateDisplay(button); 
+        updateScreen()
         
     }  })  })
 
@@ -40,38 +50,57 @@ buttons.forEach((button) => {
 function appendNum(num){
     if(operator === ""){
     firstNum += num.value;
+    // assign num to displayValue which is used to update display
+    displayValue = firstNum
     return firstNum;
     }
     else{
         secondNum += num.value;
+        // assign num to displayValue which is used to update display
+        displayValue = secondNum;
         return secondNum;
     }
 }
 // returns the value of the operator, +, -, *, /
 function getOperator(op){
-    operator = op.value;
-    return operator
+    if(!operator){
+        operator = op.value;
+        return operator;
     }
+    if((!secondOperator) && (operator)){
+        secondOperator = op.value;
+    return secondOperator;
+    }
+}
 // computes the values when 2 numbers and an operator are pressed
 function compute (num1, num2, operator){
     switch(operator){
         case "+":
             total = parseInt(num1) + parseInt(num2);
+            displayValue = total;
             return total;
         case "-":
             total = parseInt(num1) - parseInt(num2);
+            displayValue = total;
             return total;
         case "*":
             total = parseInt(num1) * parseInt(num2);
+            displayValue = total;
             return total;
         case "/":
             total = parseInt(num1) / parseInt(num2);
+            displayValue = total;
             return total;
         case "=":
             num1 = total;
+            displayValue = num1;
             num2 = "";
     }
 }
+function updateScreen(){
+    display.innerHTML = displayValue;
+}
+
 // When the clear button is clicked it deletes and resets all values
 function clear(){
 firstNum = "";
@@ -82,21 +111,6 @@ displayValue = 0;
 total = null;
 }
 
-function deleteScreen (){
-    return display.innerHTML = "0"
-}
-
-
-//display uses getElementByID instead of querySelect. This allows to update
-function updateDisplay(btn){
-    if(btn.value === "="){
-        return display.innerHTML = total;
-    }
-    else{
-        return display.innerHTML = btn.value;
-    }
-}
-
 /*
 Does the same thing as buttons.forEach
 for(let i = 0; i < buttons.length; i++) {
@@ -105,5 +119,26 @@ for(let i = 0; i < buttons.length; i++) {
             test()
         }
     })
+}
+
+//display uses getElementByID instead of querySelect. This allows to update
+function updateDisplay(btn){
+    if(btn.value === "="){
+        return display.innerHTML = total;
+    }
+    else if(!operator){
+        return display.innerHTML = firstNum;
+    }
+    else if((operator) && (!secondNum)){
+        return display.innerHTML = secondNum
+    }
+    else{
+        return display.innerHTML = btn.value;
+    }
+    display.innerHTML = displayValue;
+}
+
+function deleteScreen (){
+    return display.innerHTML = "0"
 }
 */
